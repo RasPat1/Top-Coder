d6fec5ea01eb4117c833d2a961fd76ea1ed58206
Rasesh Patel <RasPat1@gmail.com>
2014-03-16 21:51:51 -0400

    begin test runner charades

---
+++ TestRunner.java
@@ -0,0 +1,23 @@
+/*
+Each Round(folder) should contain:
+1) A sinlge .java file with one public method
+2) A corresponding compiled file (.class)
+3) A test file holding the input and outputs as KV Pairs
+
+This Test Runner will run the public method
+in each rounds class file and run the tests
+*/
+
+public class TestRunner {	
+	public static void main(String[] args) {
+		
+		/* HOW
+		Specify folder as argument
+		Get class name from .java file name
+		Create a new instance of the class 
+		Regex the .java file to get the name of the method
+		Run method on each entry of the test file.
+		Compare ... Print...
+		*/
+	}
+}
